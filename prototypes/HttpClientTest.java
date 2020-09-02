32c2b5ca4ec62fe9c24c47ba32bc5bc6c12f4f98
Andy Kawabata <46330195+andykawabata@users.noreply.github.com>
2020-09-02 10:58:46 -0400

    Update HttpClientTest.java

--- prototypes/HttpClientTest.java
+++ prototypes/HttpClientTest.java
@@ -1,27 +1,25 @@
 package http.request.test;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 /**
  *
  *Makes http request to a "quotes" api and and prints the JSON response
  *
  */
+
 public class HttpRequestTest {
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        
+    
         HttpClient httpClient = HttpClient.newHttpClient();
-
-        HttpResponse response = null;
-        response = httpClient.send(HttpRequest.newBuilder()
+        HttpResponse response = httpClient.send(HttpRequest.newBuilder()
                 .GET()
-                .uri(URI.create("https://quotes.rest/qod"))  
+                .uri(URI.create("https://sv443.net/jokeapi/v2/joke/Any?amount=10"))  
                 .build(), HttpResponse.BodyHandlers.ofString());
-
         System.out.println(response.body());
-    }    
-
+    }
+  
+    
 }

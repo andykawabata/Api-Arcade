94f516b47aae2e6736cd438afeb28dbd59b8579b
Ryan <cabell.ryan@gmail.com>
2020-09-11 15:44:15 -0400

    Added login view

---
+++ loginview/LoginView.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package loginview;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+
+public class LoginView extends Application{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("apiarcadeFXML.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("Elon Musketeers");
+        stage.setScene(scene);
+        stage.show();    }
+}
+    

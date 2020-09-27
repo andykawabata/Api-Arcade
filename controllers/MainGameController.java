e0b802b12fd0b9f0780966105f215eb966826f6b
Francisco Serratos <fjserrat@uncg.edu>
2020-09-27 14:20:53 -0400

    created main game view

---
+++ controllers/MainGameController.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author Francisco
+ */
+public class MainGameController implements Initializable {
+
+    @FXML
+    private Label userID;
+    @FXML
+    private Button moviePlayNow;
+    @FXML
+    private Label highScore;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
